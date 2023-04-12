2dc861bcaa1ab20e01393d23b575a8d76a167b8e
Nova-mist <Nova-mist@github.com>
2023-04-12 23:39:34 +0800

    initialize project

---
+++ src/test/java/com/ysama/resttemplate/RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysama.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
