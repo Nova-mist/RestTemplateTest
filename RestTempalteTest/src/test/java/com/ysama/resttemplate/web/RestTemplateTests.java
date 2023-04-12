4d9547ed8b963e5aac64b3b316c81baadb5c50a0
Nova-mist <Nova-mist@github.com>
2023-04-13 01:05:36 +0800

    testGetPlainJson

--- RestTempalteTest/src/test/java/com/ysama/resttemplate/web/RestTemplateTests.java
+++ RestTempalteTest/src/test/java/com/ysama/resttemplate/web/RestTemplateTests.java
@@ -1,9 +1,13 @@
 package com.ysama.resttemplate.web;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -25,7 +29,25 @@ public class RestTemplateTests {
 
         Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
 
-        log.warn(response.getBody());
+        log.info(response.getBody());
     }
+
+    @Test
+    void testGetPlainJson() throws JsonProcessingException {
+        String url = "http://localhost:8080/get-user";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+        log.warn(response.getBody());
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response.getBody());
+        JsonNode name = root.path("name");
+        Assertions.assertNotNull(name.asText());
+
+        log.info(name.asText());
+    }
+
 
 }
--- SampleApplication/src/main/java/com/ysama/sampleapp/controller/HelloController.java
+++ SampleApplication/src/main/java/com/ysama/sampleapp/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.ysama.sampleapp.controller;
 
+import com.ysama.sampleapp.entity.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,12 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping("/get-user")
+    public User getUser() {
+        User user = new User().setId(1001).setName("ysama");
+
+        return user;
     }
 }
